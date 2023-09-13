22765ec51014b42fdbaba528f62cc375255ad464
asaddiki <a_saddiki@etu.enset-media.ac.ma>
2023-09-13 10:55:29 -0400

    3rd commit

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -12,48 +12,47 @@ const Navigation = () => {
                     <h3>AMINE SADDIKI</h3>
                 </div>
             </div> 
+
             <div className='navigation'>
-            <ul>
-            <li>
-            <NavLink exact to="/" activeClassName="navActive">
-            <i className='fas fa-home'></i>
-            <span>Accueil</span>
-            </NavLink></li>
-            <li>
-            <NavLink exact to="/competences" activeClassName="navActive">
-            <i className='fas fa-mountain'></i>
-            <span>Compétences</span>
-            </NavLink></li>
-            <li>
-            <NavLink exact to="/portfolio" activeClassName="navActive">
-            <i className='fas fa-images'></i>
-            <span>Portfolio</span>
-            </NavLink></li>
-            <li>
-            <NavLink exact to="/contact" activeClassName="navActive">
-            <i className='fas fa-address-book'></i>
-            <span>Contact</span>
-            </NavLink></li>
-            </ul>
+                <ul>
+                    <li>
+                        <NavLink exact to="/" activeClassName="navActive">
+                            <i className='fas fa-home'></i>
+                            <span>Accueil</span>
+                        </NavLink></li>
+                    <li>
+                        <NavLink exact to="/competences" activeClassName="navActive">
+                            <i className='fas fa-mountain'></i>
+                            <span>Compétences</span>
+                        </NavLink></li>
+                    <li>
+                        <NavLink exact to="/portfolio" activeClassName="navActive">
+                            <i className='fas fa-images'></i>
+                            <span>Portfolio</span>
+                        </NavLink></li>
+                    <li>
+                        <NavLink exact to="/contact" activeClassName="navActive">
+                           <i className='fas fa-address-book'></i>
+                           <span>Contact</span>
+                        </NavLink></li>
+                </ul>
             </div>
 
             <div className='socialNetwork'>
-            <ul>
-            <li>
-            <a href='https://www.linkedin.com/in/amine-saddiki-125248284pro/' target='blank'
-            rel='nooppener noreferrer'><i className='fab fa-linkedin'></i></a>
-            </li>
-            <li>
-            <a href='https://github.com/asaddiki?tab=repositories' target='blank'
-            rel='nooppener noreferrer'><i className='fab fa-github'></i></a>
-            </li>
-            </ul>
-            <div className='signature'>
-            <p>© 2023 Amine Saddiki</p>
+                <ul>
+                    <li>
+                        <a href='https://www.linkedin.com/in/amine-saddiki-125248284pro/' target='blank'
+                        rel='nooppener noreferrer'><i className='fab fa-linkedin'></i></a>
+                    </li>
+                    <li>
+                        <a href='https://github.com/asaddiki?tab=repositories' target='blank'
+                        rel='nooppener noreferrer'><i className='fab fa-github'></i></a>
+                    </li>
+                </ul>
+                <div className='signature'>
+                    <p>© 2023 Amine Saddiki</p>
+                </div>
             </div>
-
-            </div>
-
         </div>
     );
 };
